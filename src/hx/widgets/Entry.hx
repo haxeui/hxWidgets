881fa4e6fd68c9945605910b5280a7214e00e911
Valentin Lemière <me@ibilon.com>
2016-01-11 21:41:25 +0100

    Move the build configuration to a file, add linux configuration

--- src/hx/widgets/Entry.hx
+++ src/hx/widgets/Entry.hx
@@ -1,24 +1,6 @@
 package hx.widgets;
 
-@:buildXml('
-<files id="haxe">
-	<compilerflag value="-IZ:/hxWidgets/wxWidgets/include" />
-</files>
-<target id="haxe" tool="linker" toolid="exe">
-	<lib name="Z:/hxWidgets/lib/Windows/libwxwidgets.lib" />
-	<lib name="user32.lib" />
-	<lib name="winspool.lib" />
-	<lib name="advapi32.lib" />
-	<lib name="gdi32.lib" />
-	<lib name="comdlg32.lib" />
-	<lib name="comctl32.lib" />
-	<lib name="ole32.lib" />
-	<lib name="oleaut32.lib" />
-	<lib name="rpcrt4.lib" />
-	<lib name="shell32.lib" />
-	<lib name="kernel32.lib" />
-</target>
-')
+@:buildXml('<include name="./../../../Build.xml" />')
 @:headerCode("
 #include <wx/wx.h>
 #undef RegisterClass
