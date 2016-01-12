c47a6f339f4553c3e3df71df4c80b22238a2a5c1
Ian Harrigan <ianharrigan@hotmail.com>
2016-01-12 13:13:54 +0100

    tabs to spaces

--- src/hx/widgets/Slider.hx
+++ src/hx/widgets/Slider.hx
@@ -8,20 +8,20 @@ import hx.widgets.Window.WxWindowRef;
 
 @:headerCode("#include <wx/slider.h>")
 class SliderStyle {
-	public static var HORIZONTAL:Int = 		untyped __cpp__("wxSL_HORIZONTAL"); 	// Displays the slider horizontally (this is the default).
-	public static var VERTICAL:Int = 		untyped __cpp__("wxSL_VERTICAL");		// Displays the slider vertically.
-	public static var AUTOTICKS:Int = 		untyped __cpp__("wxSL_AUTOTICKS");		// Displays tick marks. Windows only.
-	public static var MIN_MAX_LABELS:Int = 	untyped __cpp__("wxSL_MIN_MAX_LABELS");	// Displays minimum, maximum labels (new since wxWidgets 2.9.1).
-	public static var VALUE_LABEL:Int = 	untyped __cpp__("wxSL_VALUE_LABEL");	// Displays value label (new since wxWidgets 2.9.1).
-	public static var LABELS:Int = 			untyped __cpp__("wxSL_LABELS");			// Displays minimum, maximum and value labels (same as wxSL_VALUE_LABEL and wxSL_MIN_MAX_LABELS together).
-	public static var LEFT:Int = 			untyped __cpp__("wxSL_LEFT");			// Displays ticks on the left and forces the slider to be vertical.
-	public static var RIGHT:Int = 			untyped __cpp__("wxSL_RIGHT");			// Displays ticks on the right and forces the slider to be vertical.
-	public static var TOP:Int = 			untyped __cpp__("wxSL_TOP");			// Displays ticks on the top.
-	public static var BOTTOM:Int = 			untyped __cpp__("wxSL_BOTTOM");			// Displays ticks on the bottom (this is the default).
-	public static var SELRANGE:Int = 		untyped __cpp__("wxSL_SELRANGE");		// Allows the user to select a range on the slider. Windows only.
-	public static var INVERSE:Int = 		untyped __cpp__("wxSL_INVERSE");		// Inverses the minimum and maximum endpoints on the slider. Not compatible with wxSL_SELRANGE.
+	public static var HORIZONTAL:Int =      untyped __cpp__("wxSL_HORIZONTAL");     // Displays the slider horizontally (this is the default).
+	public static var VERTICAL:Int =        untyped __cpp__("wxSL_VERTICAL");       // Displays the slider vertically.
+	public static var AUTOTICKS:Int =       untyped __cpp__("wxSL_AUTOTICKS");      // Displays tick marks. Windows only.
+	public static var MIN_MAX_LABELS:Int =  untyped __cpp__("wxSL_MIN_MAX_LABELS"); // Displays minimum, maximum labels (new since wxWidgets 2.9.1).
+	public static var VALUE_LABEL:Int =     untyped __cpp__("wxSL_VALUE_LABEL");    // Displays value label (new since wxWidgets 2.9.1).
+	public static var LABELS:Int =          untyped __cpp__("wxSL_LABELS");         // Displays minimum, maximum and value labels (same as wxSL_VALUE_LABEL and wxSL_MIN_MAX_LABELS together).
+	public static var LEFT:Int =            untyped __cpp__("wxSL_LEFT");           // Displays ticks on the left and forces the slider to be vertical.
+	public static var RIGHT:Int =           untyped __cpp__("wxSL_RIGHT");          // Displays ticks on the right and forces the slider to be vertical.
+	public static var TOP:Int =             untyped __cpp__("wxSL_TOP");            // Displays ticks on the top.
+	public static var BOTTOM:Int =          untyped __cpp__("wxSL_BOTTOM");         // Displays ticks on the bottom (this is the default).
+	public static var SELRANGE:Int =        untyped __cpp__("wxSL_SELRANGE");       // Allows the user to select a range on the slider. Windows only.
+	public static var INVERSE:Int =         untyped __cpp__("wxSL_INVERSE");        // Inverses the minimum and maximum endpoints on the slider. Not compatible with wxSL_SELRANGE.
 	
-	public static var DEFAULT:Int = 		HORIZONTAL;
+	public static var DEFAULT:Int =         HORIZONTAL;
 }
 
 class Slider extends Window {
@@ -98,20 +98,20 @@ class Slider extends Window {
 @:include("wx/slider.h")
 @:native("cpp.Reference<wxSlider>")
 extern class WxSliderRef extends WxSlider {
-	@:native("new wxSlider") 	public static function createInstance():WxSliderRef;
+	@:native("new wxSlider")    public static function createInstance():WxSliderRef;
 }
 
 @:include("wx/slider.h")
 @:native("wxSlider")
 extern class WxSlider extends WxWindow {
-	@:native("Create") 			public function create(parent:WxWindowRef, id:Int, value:Int, minValue:Int, maxValue:Int, point:WxPointRef, size:WxSizeRef, style:Int):Bool;
-	@:native("SetValue") 		public function setValue(value:Int):Void;
-	@:native("GetValue") 		public function getValue():Int;
-	@:native("SetMin") 			public function setMin(value:Int):Void;
-	@:native("GetMin") 			public function getMin():Int;
-	@:native("SetMax") 			public function setMax(value:Int):Void;
-	@:native("GetMax") 			public function getMax():Int;
-	@:native("SetSelection")	public function setSelection(startPos:Int, endPos:Int):Void;
-	@:native("GetSelStart") 	public function getSelStart():Int;
-	@:native("GetSelEnd") 		public function getSelEnd():Int;
+	@:native("Create")          public function create(parent:WxWindowRef, id:Int, value:Int, minValue:Int, maxValue:Int, point:WxPointRef, size:WxSizeRef, style:Int):Bool;
+	@:native("SetValue")        public function setValue(value:Int):Void;
+	@:native("GetValue")        public function getValue():Int;
+	@:native("SetMin")          public function setMin(value:Int):Void;
+	@:native("GetMin")          public function getMin():Int;
+	@:native("SetMax")          public function setMax(value:Int):Void;
+	@:native("GetMax")          public function getMax():Int;
+	@:native("SetSelection")    public function setSelection(startPos:Int, endPos:Int):Void;
+	@:native("GetSelStart")     public function getSelStart():Int;
+	@:native("GetSelEnd")       public function getSelEnd():Int;
 }
