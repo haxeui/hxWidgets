29419825c1e9a45a00608e7ce7400bb110569428
Ian Harrigan <ianharrigan@hotmail.com>
2016-01-12 12:31:53 +0100

    Add slider styles

---
+++ src/hx/widgets/Size.hx
@@ -0,0 +1,56 @@
+package hx.widgets;
+
+class Size {
+	private var _ref:WxSizeRef;
+	public function new(width:Int = 0, height:Int = 0) {
+		_ref = WxSizeRef.createInstance();
+		_ref.setWidth(width);
+		_ref.setHeight(height);
+	}
+	
+	public var width(get, set):Int;
+	private function get_width():Int {
+		return _ref.getWidth();
+	}
+	private function set_width(value:Int):Int {
+		_ref.setWidth(value);
+		return value;
+	}
+	
+	public var height(get, set):Int;
+	private function get_height():Int {
+		return _ref.getHeight();
+	}
+	private function set_height(value:Int):Int {
+		_ref.setHeight(value);
+		return value;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// HELPERS
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static var defaultSize(get, null):Size;
+	private static function get_defaultSize():Size {
+		return new Size(-1, -1);
+	}
+	
+	public static var defaultSizeRef(get, null):WxSizeRef;
+	private static function get_defaultSizeRef():WxSizeRef {
+		return defaultSize._ref;
+	}
+}
+
+@:include("wx/gdicmn.h")
+@:native("cpp.Reference<wxSize>")
+extern class WxSizeRef extends WxSize {
+	@:native("new wxSize") public static function createInstance():WxSizeRef;
+}
+
+@:include("wx/gdicmn.h")
+@:native("wxSize")
+extern class WxSize {
+	@:native("GetWidth") public function getWidth():Int;
+	@:native("GetHeight") public function getHeight():Int;
+	@:native("SetWidth") public function setWidth(value:Int):Void;
+	@:native("SetHeight") public function setHeight(value:Int):Void;
+}
--- src/hx/widgets/Slider.hx
+++ src/hx/widgets/Slider.hx
@@ -1,15 +1,39 @@
 package hx.widgets;
 
 import cpp.ConstCharStar;
+import hx.widgets.Point.WxPointRef;
+import hx.widgets.Size.WxSizeRef;
 import hx.widgets.Window.WxWindow;
 import hx.widgets.Window.WxWindowRef;
 
+@:headerCode("#include <wx/slider.h>")
+class SliderStyle {
+	public static var HORIZONTAL:Int = 		untyped __cpp__("wxSL_HORIZONTAL"); 	// Displays the slider horizontally (this is the default).
+	public static var VERTICAL:Int = 		untyped __cpp__("wxSL_VERTICAL");		// Displays the slider vertically.
+	public static var AUTOTICKS:Int = 		untyped __cpp__("wxSL_AUTOTICKS");		// Displays tick marks. Windows only.
+	public static var MIN_MAX_LABELS:Int = 	untyped __cpp__("wxSL_MIN_MAX_LABELS");	// Displays minimum, maximum labels (new since wxWidgets 2.9.1).
+	public static var VALUE_LABEL:Int = 	untyped __cpp__("wxSL_VALUE_LABEL");	// Displays value label (new since wxWidgets 2.9.1).
+	public static var LABELS:Int = 			untyped __cpp__("wxSL_LABELS");			// Displays minimum, maximum and value labels (same as wxSL_VALUE_LABEL and wxSL_MIN_MAX_LABELS together).
+	public static var LEFT:Int = 			untyped __cpp__("wxSL_LEFT");			// Displays ticks on the left and forces the slider to be vertical.
+	public static var RIGHT:Int = 			untyped __cpp__("wxSL_RIGHT");			// Displays ticks on the right and forces the slider to be vertical.
+	public static var TOP:Int = 			untyped __cpp__("wxSL_TOP");			// Displays ticks on the top.
+	public static var BOTTOM:Int = 			untyped __cpp__("wxSL_BOTTOM");			// Displays ticks on the bottom (this is the default).
+	public static var SELRANGE:Int = 		untyped __cpp__("wxSL_SELRANGE");		// Allows the user to select a range on the slider. Windows only.
+	public static var INVERSE:Int = 		untyped __cpp__("wxSL_INVERSE");		// Inverses the minimum and maximum endpoints on the slider. Not compatible with wxSL_SELRANGE.
+	
+	public static var DEFAULT:Int = 		HORIZONTAL;
+}
+
 class Slider extends Window {
-	public function new(parent:Window, value:Int = 0, min:Int = 0, max:Int = 100, id:Int = -1) {
+	public function new(parent:Window, value:Int = 0, min:Int = 0, max:Int = 100, style:Int = -1, id:Int = -1) {
 		super(parent, id);
+
+		if (style == -1) {
+			style = SliderStyle.DEFAULT;
+		}
 		
 		var sliderRef:WxSliderRef = WxSliderRef.createInstance();
-		sliderRef.create(parent != null ? parent._ref : Window.nullWindowRef, id, value, min, max);
+		sliderRef.create(parent != null ? parent._ref : Window.nullWindowRef, id, value, min, max, Point.defaultPositionRef, Size.defaultSizeRef, style);
 		_ref = cast sliderRef;
 	}
 	
@@ -40,6 +64,28 @@ class Slider extends Window {
 		return value;
 	}
 	
+	public function setSelection(startPos:Int, endPos:Int) {
+		sliderRef.setSelection(startPos, endPos);
+	}
+	
+	public var selectionStart(get, set):Int;
+	private function get_selectionStart():Int {
+		return sliderRef.getSelStart();
+	}
+	private function set_selectionStart(value:Int):Int {
+		setSelection(value, selectionEnd);
+		return value;
+	}
+
+	public var selectionEnd(get, set):Int;
+	private function get_selectionEnd():Int {
+		return sliderRef.getSelEnd();
+	}
+	private function set_selectionEnd(value:Int):Int {
+		setSelection(selectionStart, value);
+		return value;
+	}
+	
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// HELPERS
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -52,17 +98,20 @@ class Slider extends Window {
 @:include("wx/slider.h")
 @:native("cpp.Reference<wxSlider>")
 extern class WxSliderRef extends WxSlider {
-	@:native("new wxSlider") public static function createInstance():WxSliderRef;
+	@:native("new wxSlider") 	public static function createInstance():WxSliderRef;
 }
 
 @:include("wx/slider.h")
 @:native("wxSlider")
 extern class WxSlider extends WxWindow {
-	@:native("Create") public function create(parent:WxWindowRef, id:Int, value:Int, minValue:Int, maxValue:Int):Bool;
-	@:native("SetValue") public function setValue(value:Int):Void;
-	@:native("GetValue") public function getValue():Int;
-	@:native("SetMin") public function setMin(value:Int):Void;
-	@:native("GetMin") public function getMin():Int;
-	@:native("SetMax") public function setMax(value:Int):Void;
-	@:native("GetMax") public function getMax():Int;
+	@:native("Create") 			public function create(parent:WxWindowRef, id:Int, value:Int, minValue:Int, maxValue:Int, point:WxPointRef, size:WxSizeRef, style:Int):Bool;
+	@:native("SetValue") 		public function setValue(value:Int):Void;
+	@:native("GetValue") 		public function getValue():Int;
+	@:native("SetMin") 			public function setMin(value:Int):Void;
+	@:native("GetMin") 			public function getMin():Int;
+	@:native("SetMax") 			public function setMax(value:Int):Void;
+	@:native("GetMax") 			public function getMax():Int;
+	@:native("SetSelection")	public function setSelection(startPos:Int, endPos:Int):Void;
+	@:native("GetSelStart") 	public function getSelStart():Int;
+	@:native("GetSelEnd") 		public function getSelEnd():Int;
 }
