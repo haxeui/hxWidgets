0bea27fe70079734e006fdecd9a8ad5a98e73447
Valentin Lemière <me@ibilon.com>
2016-02-18 13:02:16 +0100

    Use wx-config on linux and mac

--- src/wx/widgets/Entry.hx
+++ src/wx/widgets/Entry.hx
@@ -1,11 +1,15 @@
 package wx.widgets;
 
-@:buildXml('<include name="${haxelib:hxWidgets}/../Build.xml" />')
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
+#if !macro @:build(wx.widgets.Entry.wxconfig()) #end
 @:headerCode("
 #include <wx/wx.h>
 #undef RegisterClass
 ")
 class Entry {
+	#if !macro
     public static function start(args:Array<String>):Void {
         var args = [Sys.executablePath()].concat(args);
         var argc = args.length;
@@ -22,5 +26,41 @@ class Entry {
 
     public static function cleanup ():Void {
         untyped __cpp__("wxEntryCleanup();");
+    }
+    #end
+
+    macro public static function wxconfig():Array<Field> {
+        var _pos = Context.currentPos();
+        var _class = Context.getLocalClass();
+
+        if (new EReg("window", "i").match(Sys.systemName())) {
+			_class.get().meta.add(":buildXml", [{ expr:EConst( CString( "<include name=\"${haxelib:hxWidgets}/../Build.xml\" />" ) ), pos:_pos }], _pos );
+		} else {
+			var config = new sys.io.Process("wx-config", ["--cflags"]);
+			var cflags = config.stdout.readAll().toString().split("\n")[0].split(" ").map(function (s:String) {
+				return '<compilerflag value="$s" />';
+			}).join("\n");
+			config.exitCode();
+
+			var config = new sys.io.Process("wx-config", ["--libs"]);
+			var libs = config.stdout.readAll().toString().split("\n")[0].split(" ");
+			var link = [];
+			var i = 0;
+			while (i < libs.length) {
+				if (libs[i] == "-framework") {
+					link.push('<vflag name="${libs[i]}" value="${libs[i+1]}"/>');
+					i++;
+				} else {
+					link.push('<lib name="${libs[i]}" />');
+				}
+
+				i++;
+			}
+			config.exitCode();
+
+			_class.get().meta.add(":buildXml", [{ expr:EConst( CString( '<files id="haxe">$cflags</files>\n<target id="haxe" tool="linker" toolid="exe">${link.join("\n")}</target>' ) ), pos:_pos }], _pos );
+        }
+
+        return Context.getBuildFields();
     }
 }
