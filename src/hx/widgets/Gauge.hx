170d0c2bfa0bc46cd603564a7b5c54e2a39eda30
Ian Harrigan <ianharrigan@hotmail.com>
2016-02-20 23:38:18 +0100

    Allow style to be passed into guage

--- src/hx/widgets/Gauge.hx
+++ src/hx/widgets/Gauge.hx
@@ -1,12 +1,16 @@
 package hx.widgets;
 
 import wx.widgets.Gauge in WxGauge;
+import hx.widgets.styles.GaugeStyle;
 
 class Gauge extends Control {
-    public function new(parent:Window, range:Int = 100, id:Int = -1) {
+    public function new(parent:Window, range:Int = 100, style:Int = -1, id:Int = -1) {
+        if (style == -1) {
+            style = GaugeStyle.HORIZONTAL;
+        }
         if (_ref == null) {
             var guageRef:WxGauge = WxGauge.createInstance();
-            guageRef.create(parent != null ? parent._ref : Window.nullWindowRef, id, range);
+            guageRef.create(parent != null ? parent._ref : Window.nullWindowRef, id, range, Point.defaultPositionRef, Size.defaultSizeRef, style);
             _ref = guageRef;
         }
         
