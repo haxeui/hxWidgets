554b22e7c053904d8ea46a7321f59a2732fe10fe
Ian Harrigan <ianharrigan@hotmail.com>
2016-02-21 19:16:55 +0100

    Changed ItemKind extern method

--- src/wx/widgets/ItemKind.hx
+++ src/wx/widgets/ItemKind.hx
@@ -1,12 +1,22 @@
 package wx.widgets;
 
 @:include("wx/menu.h")
-@:unreflective
 @:native("cpp::Reference<wxItemKind>")
-extern class ItemKind {
-    @:native("wxITEM_SEPARATOR") public static var ITEM_SEPARATOR:ItemKind;
-    @:native("wxITEM_NORMAL")    public static var ITEM_NORMAL:ItemKind;
-    @:native("wxITEM_CHECK")     public static var ITEM_CHECK:ItemKind;
-    @:native("wxITEM_RADIO")     public static var ITEM_RADIO:ItemKind;
-    @:native("wxITEM_DROPDOWN")  public static var ITEM_DROPDOWN:ItemKind;
+extern class ItemKindType {
+}
+
+@:headerCode("#include <wx/menu.h>")
+abstract ItemKind(Int) {
+    public static var ITEM_SEPARATOR(default, null)          = new ItemKind(untyped __cpp__('::wxITEM_SEPARATOR')); 
+    public static var ITEM_NORMAL(default, null)             = new ItemKind(untyped __cpp__('::wxITEM_NORMAL')); 
+    public static var ITEM_CHECK(default, null)              = new ItemKind(untyped __cpp__('::wxITEM_CHECK')); 
+    public static var ITEM_RADIO(default, null)              = new ItemKind(untyped __cpp__('::wxITEM_RADIO')); 
+    public static var ITEM_DROPDOWN(default, null)           = new ItemKind(untyped __cpp__('::wxITEM_DROPDOWN')); 
+    
+    inline public function new(i:Int) {
+        this = i;
+    }
+    
+    @:to @:unreflective
+    inline public function toNative():ItemKindType return untyped __cpp__("static_cast<wxItemKind>({0})", this);
 }
