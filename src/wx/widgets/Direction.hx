25ced270aec057d767b2b482e5be29d7b5310759
Ian Harrigan <ianharrigan@hotmail.com>
2016-02-21 19:48:01 +0100

    Remove namespaces from all enums

--- src/wx/widgets/Direction.hx
+++ src/wx/widgets/Direction.hx
@@ -2,17 +2,17 @@ package wx.widgets;
 
 @:headerCode("#include <wx/defs.h>")
 abstract Direction(Int) {
-    public static var LEFT(default, null)         = new Direction(untyped __cpp__('wxDirection::wxLEFT')); 
-    public static var RIGHT(default, null)        = new Direction(untyped __cpp__('wxDirection::wxRIGHT')); 
-    public static var UP(default, null)           = new Direction(untyped __cpp__('wxDirection::wxUP')); 
-    public static var DOWN(default, null)         = new Direction(untyped __cpp__('wxDirection::wxDOWN')); 
-    public static var TOP(default, null)          = new Direction(untyped __cpp__('wxDirection::wxTOP')); 
-    public static var BOTTOM(default, null)       = new Direction(untyped __cpp__('wxDirection::wxBOTTOM')); 
-    public static var NORTH(default, null)        = new Direction(untyped __cpp__('wxDirection::wxNORTH')); 
-    public static var SOUTH(default, null)        = new Direction(untyped __cpp__('wxDirection::wxSOUTH')); 
-    public static var WEST(default, null)         = new Direction(untyped __cpp__('wxDirection::wxWEST')); 
-    public static var EAST(default, null)         = new Direction(untyped __cpp__('wxDirection::wxEAST')); 
-    public static var ALL(default, null)          = new Direction(untyped __cpp__('wxDirection::wxALL')); 
+    public static var LEFT(default, null)         = new Direction(untyped __cpp__('::wxLEFT')); 
+    public static var RIGHT(default, null)        = new Direction(untyped __cpp__('::wxRIGHT')); 
+    public static var UP(default, null)           = new Direction(untyped __cpp__('::wxUP')); 
+    public static var DOWN(default, null)         = new Direction(untyped __cpp__('::wxDOWN')); 
+    public static var TOP(default, null)          = new Direction(untyped __cpp__('::wxTOP')); 
+    public static var BOTTOM(default, null)       = new Direction(untyped __cpp__('::wxBOTTOM')); 
+    public static var NORTH(default, null)        = new Direction(untyped __cpp__('::wxNORTH')); 
+    public static var SOUTH(default, null)        = new Direction(untyped __cpp__('::wxSOUTH')); 
+    public static var WEST(default, null)         = new Direction(untyped __cpp__('::wxWEST')); 
+    public static var EAST(default, null)         = new Direction(untyped __cpp__('::wxEAST')); 
+    public static var ALL(default, null)          = new Direction(untyped __cpp__('::wxALL')); 
     
     inline public function new(i:Int) {
         this = i;
