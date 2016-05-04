168c6a4cda64de29e6f7a9e317bd3598405323b4
Valentin Lemière <me@ibilon.com>
2016-05-04 22:26:17 +0200

    Base haxe 3.3 extern enums on Int

--- src/wx/widgets/AntialiasMode.hx
+++ src/wx/widgets/AntialiasMode.hx
@@ -3,23 +3,16 @@ package wx.widgets;
 #if (haxe_ver >= 3.3)
 @:headerCode("#include <wx/graphics.h>")
 @:unreflective
-@:native("wxAntialiasMode")
-@:enum extern abstract AntialiasMode(AntialiasModeImpl) {
+@:enum extern abstract AntialiasMode(Int) to Int from Int {
     @:native("wxANTIALIAS_NONE")        var NONE;
     @:native("wxANTIALIAS_DEFAULT")     var DEFAULT;
-}
-
-@:headerCode("#include <wx/graphics.h>")
-@:unreflective
-@:native("wxAntialiasMode")
-extern class AntialiasModeImpl {
 }
 #else
 @:headerCode("#include <wx/graphics.h>")
 abstract AntialiasMode(Int) {
-    public static var NONE(default, null)           = new AntialiasMode(untyped __cpp__('::wxANTIALIAS_NONE')); 
-    public static var DEFAULT(default, null)        = new AntialiasMode(untyped __cpp__('::wxANTIALIAS_DEFAULT')); 
-    
+    public static var NONE(default, null)           = new AntialiasMode(untyped __cpp__('::wxANTIALIAS_NONE'));
+    public static var DEFAULT(default, null)        = new AntialiasMode(untyped __cpp__('::wxANTIALIAS_DEFAULT'));
+
     inline public function new(i:Int) {
         this = i;
     }
--- src/wx/widgets/Direction.hx
+++ src/wx/widgets/Direction.hx
@@ -3,8 +3,7 @@ package wx.widgets;
 #if (haxe_ver >= 3.3)
 @:headerCode("#include <wx/defs.h>")
 @:unreflective
-@:native("wxDirection")
-@:enum extern abstract Direction(DirectionImpl) from Int to Int {
+@:enum extern abstract Direction(Int) to Int from Int {
     @:native("wxLEFT")   var LEFT;
     @:native("wxRIGHT")  var RIGHT;
     @:native("wxUP")     var UP;
@@ -17,27 +16,21 @@ package wx.widgets;
     @:native("wxEAST")   var EAST;
     @:native("wxALL")    var ALL;
 }
-
-@:headerCode("#include <wx/defs.h>")
-@:unreflective
-@:native("wxDirection")
-extern class DirectionImpl {
-}
 #else
 @:headerCode("#include <wx/defs.h>")
 abstract Direction(Int) from Int to Int {
-    public static var LEFT(default, null)         = new Direction(untyped __cpp__('::wxLEFT')); 
-    public static var RIGHT(default, null)        = new Direction(untyped __cpp__('::wxRIGHT')); 
-    public static var UP(default, null)           = new Direction(untyped __cpp__('::wxUP')); 
-    public static var DOWN(default, null)         = new Direction(untyped __cpp__('::wxDOWN')); 
-    public static var TOP(default, null)          = new Direction(untyped __cpp__('::wxTOP')); 
-    public static var BOTTOM(default, null)       = new Direction(untyped __cpp__('::wxBOTTOM')); 
-    public static var NORTH(default, null)        = new Direction(untyped __cpp__('::wxNORTH')); 
-    public static var SOUTH(default, null)        = new Direction(untyped __cpp__('::wxSOUTH')); 
-    public static var WEST(default, null)         = new Direction(untyped __cpp__('::wxWEST')); 
-    public static var EAST(default, null)         = new Direction(untyped __cpp__('::wxEAST')); 
-    public static var ALL(default, null)          = new Direction(untyped __cpp__('::wxALL')); 
-    
+    public static var LEFT(default, null)         = new Direction(untyped __cpp__('::wxLEFT'));
+    public static var RIGHT(default, null)        = new Direction(untyped __cpp__('::wxRIGHT'));
+    public static var UP(default, null)           = new Direction(untyped __cpp__('::wxUP'));
+    public static var DOWN(default, null)         = new Direction(untyped __cpp__('::wxDOWN'));
+    public static var TOP(default, null)          = new Direction(untyped __cpp__('::wxTOP'));
+    public static var BOTTOM(default, null)       = new Direction(untyped __cpp__('::wxBOTTOM'));
+    public static var NORTH(default, null)        = new Direction(untyped __cpp__('::wxNORTH'));
+    public static var SOUTH(default, null)        = new Direction(untyped __cpp__('::wxSOUTH'));
+    public static var WEST(default, null)         = new Direction(untyped __cpp__('::wxWEST'));
+    public static var EAST(default, null)         = new Direction(untyped __cpp__('::wxEAST'));
+    public static var ALL(default, null)          = new Direction(untyped __cpp__('::wxALL'));
+
     inline public function new(i:Int) {
         this = i;
     }
