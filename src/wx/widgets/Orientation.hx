ab2f5e55c22f64d54d073007b6f0db9b8ff3a8bc
Valentin Lemière <me@ibilon.com>
2016-05-05 15:35:13 +0200

    Fix Orientation on haxe 3.2

--- src/wx/widgets/Orientation.hx
+++ src/wx/widgets/Orientation.hx
@@ -12,8 +12,8 @@ package wx.widgets;
 extern class OrientationImpl {
 }
 #else
-@:include("wx/window.h")
-abstract Orientation(Int) {
+@:headerCode("#include <wx/window.h>")
+abstract Orientation(Int) to Int from Int {
     public static var VERTICAL:Int                = untyped __cpp__("wxVERTICAL");
     public static var HORIZONTAL:Int              = untyped __cpp__("wxHORIZONTAL");
 }
