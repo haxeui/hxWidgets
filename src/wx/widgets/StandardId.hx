f4ec97d5f9489ed6c230f92563b893c08b24c573
Ian Harrigan <ianharrigan@hotmail.com>
2016-05-15 14:22:33 +0200

    Remove namespace from standardid

--- src/wx/widgets/StandardId.hx
+++ src/wx/widgets/StandardId.hx
@@ -2,7 +2,7 @@ package wx.widgets;
 
 @:headerCode("#include <wx/defs.h>")
 class StandardId {
-    public static var OK:Int            = untyped __cpp__("wxStandardID::wxID_OK");
+    public static var OK:Int            = untyped __cpp__("::wxID_OK");
     /*
     public static var ABOUT:Int         = untyped __cpp__("wxStandardID::wxID_ABOUT");
     public static var PREFERENCES:Int   = untyped __cpp__("wxStandardID::wxID_PREFERENCES");
