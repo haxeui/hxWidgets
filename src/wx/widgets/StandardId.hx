53d01cc1e62b81313de319d3ca33b3119b0a7bb7
Ian Harrigan <ianharrigan@hotmail.com>
2018-11-16 09:39:07 +0100

    add more standardIds

--- src/wx/widgets/StandardId.hx
+++ src/wx/widgets/StandardId.hx
@@ -2,13 +2,12 @@ package wx.widgets;
 
 @:headerCode("#include <wx/defs.h>")
 class StandardId {
-
-    public static var OK:Int            = untyped __cpp__("::wxID_OK");
-
-    /*
-    public static var ABOUT:Int         = untyped __cpp__("wxStandardID::wxID_ABOUT");
-    public static var PREFERENCES:Int   = untyped __cpp__("wxStandardID::wxID_PREFERENCES");
-    public static var EXIT:Int          = untyped __cpp__("wxStandardID::wxID_EXIT");
-    */
-
+    public static var NONE:Int      = untyped __cpp__("::wxID_NONE");
+    public static var CLOSE:Int     = untyped __cpp__("::wxID_CLOSE");
+    public static var OK:Int        = untyped __cpp__("::wxID_OK");
+    public static var SAVE:Int      = untyped __cpp__("::wxID_SAVE");
+    public static var YES:Int       = untyped __cpp__("::wxID_YES");
+    public static var NO:Int        = untyped __cpp__("::wxID_NO");
+    public static var CANCEL:Int    = untyped __cpp__("::wxID_CANCEL");
+    public static var APPLY:Int     = untyped __cpp__("::wxID_APPLY");
 }
