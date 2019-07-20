2321baa117e0a4710aa03642224cc8cba39bf546
Ian Harrigan <ianharrigan@hotmail.com>
2019-07-20 17:34:05 +0200

    haxelib

--- submit.bat
+++ submit.bat
@@ -1,3 +1,3 @@
 del haxelib.zip
-7z a haxelib.zip *.hx *.h *.manifest -r LICENCE.md README.md haxelib.json Build.xml
-haxelib submit haxelib.zip
+7z a haxelib.zip *.hx *.h *.cpp *.manifest *.png *.hxml *.hxproj *.bat -r LICENCE.md README.md haxelib.json Build.xml -xr!bin
+REM haxelib submit haxelib.zip
