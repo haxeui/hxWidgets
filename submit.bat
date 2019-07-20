9c203df8dcc99a9b795552a9e88336dd62bb7ce0
Ian Harrigan <ianharrigan@hotmail.com>
2019-07-20 17:18:22 +0200

    haxelib fix

--- submit.bat
+++ submit.bat
@@ -1,3 +1,3 @@
 del haxelib.zip
-7z a haxelib.zip *.hx *.h *.manifest -r LICENCE.md README.md haxelib.json
+7z a haxelib.zip *.hx *.h *.manifest -r LICENCE.md README.md haxelib.json Build.xml
 haxelib submit haxelib.zip
