1e84414e4b04e8726d85a34d0f952a91c59c7695
Ian Harrigan <ianharrigan@hotmail.com>
2019-07-20 17:34:56 +0200

    remove REM

--- submit.bat
+++ submit.bat
@@ -1,3 +1,3 @@
 del haxelib.zip
 7z a haxelib.zip *.hx *.h *.cpp *.manifest *.png *.hxml *.hxproj *.bat -r LICENCE.md README.md haxelib.json Build.xml -xr!bin
-REM haxelib submit haxelib.zip
+haxelib submit haxelib.zip
