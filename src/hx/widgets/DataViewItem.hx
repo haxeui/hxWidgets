14813c03dd03a264952a12474660863af5756b97
Ian Harrigan <ianharrigan@hotmail.com>
2022-08-30 10:40:22 +0200

    DataViewItem::IsOK / DataViewItem::GetID

--- src/hx/widgets/DataViewItem.hx
+++ src/hx/widgets/DataViewItem.hx
@@ -1,11 +1,25 @@
 package hx.widgets;
 
-import cpp.Pointer;
 import wx.widgets.DataViewItem in WxDataViewItem;
 
 class DataViewItem {
     private var _item:WxDataViewItem;
     
     public function new() {
+    }
+    
+    public var id(get, null):Int;
+    private function get_id():Int {
+        if (!isOK) {
+            return -1;
+        }
+        var id = _item.getId();
+        var v = untyped __cpp__("(int){0}", id);
+        return v;
+    }
+    
+    public var isOK(get, null):Bool;
+    private function get_isOK():Bool {
+        return _item.isOK();
     }
 }
--- src/wx/widgets/DataViewItem.hx
+++ src/wx/widgets/DataViewItem.hx
@@ -1,8 +1,11 @@
 package wx.widgets;
+import cpp.RawPointer;
 
 @:include("wx/dataview.h")
 @:unreflective
 @:native("wxDataViewItem")
 @:structAccess
 extern class DataViewItem {
+    @:native("GetID")           public function getId():RawPointer<cpp.Void>;
+    @:native("IsOk")            public function isOK():Bool;
 }
