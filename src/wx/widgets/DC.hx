baab03502286022ef03a86822317627158828df0
shallowmallow <maud.davout@gmail.com>
2023-08-19 20:44:07 +0200

    DC bindings

--- src/wx/widgets/DC.hx
+++ src/wx/widgets/DC.hx
@@ -9,6 +9,23 @@ extern class DC extends Object {
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Instance functions
     //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    @:native("CalcBoundingBox")         public function calcBoundingBox(x:Int, y:Int):Void;
+    @:native("CanDrawBitmap")           public function canDrawBitmap():Bool;
+    @:native("CanGetTextEvent")         public function canGetTextEvent():Bool;
+    @:native("CanUserTransformMatrix")  public function canUserTransformMatrix():Bool;
+    @:native("CopyAttributes")          public function copyAttributes(dc:DC):Void;
+    @:native("CrossHair")               public function crossHair(x:Int, y:Int):Void;
+    @:native("DestroyClippingRegion")   public function destroyClippingRegion():Void;
+    @:native("DeviceToLogical")         public function deviceToLogical(x:Int, y:Int):Point;
+    @:native("DeviceToLogicalRel")      public function deviceToLogicalRel(x:Int, y:Int):Size; 
+    @:native("DrawArc")                 public function drawArc(xStart:Int, yStart:Int, xEnd:Int, yEnd:Int, xc:Int, yc:Int):Void;
+    @:native("DrawCheckMark")           public function drawCheckMark(x:Int, y:Int, width:Int, height:Int):Void;
+    @:native("DrawEllipse")             public function drawEllipse(x:Int, y:Int, width:Int, height:Int):Void;
+    @:native("DrawIcon")                public function drawIcon(icon:Icon, x:Int, y:Int):Void;
+    @:native("DrawPoint")               public function drawPoint(x:Int, y:Int):Void;
+    @:native("DrawRectangle")           public function drawRectangle(x:Int, y:Int, width:Int, height:Int):Void;
+    @:native("GetPixel")                public function getPixel(x:Int, y:Int, colour:Colour):Bool;
     @:native("Clear")                   public function clear():Void;
     @:native("DrawLine")                public function drawLine(x1:Int, y1:Int, x2:Int, y2:Int):Void;
     @:native("SetPen")                  public function setPen(pen:Pen):Void;
